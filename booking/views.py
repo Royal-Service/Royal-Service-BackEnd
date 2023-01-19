b508f9345b4ac79b775fe53d4f28da833887dcb7
IsmailAlamir <ismaeelalamir@gmail.com>
2023-01-19 20:00:45 +0300

    EDIT Booking URL

--- booking/views.py
+++ booking/views.py
@@ -26,21 +26,23 @@ def isCraftsmanValid(craftsman):
     except:
         return False
 
-class BookingListCreateAPIView(generics.ListCreateAPIView):
+class BookingListCreateAPIView(generics.ListAPIView):
+    # ListCreateAPIView
+
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
 class BookingRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Booking.objects.all()
-    serializer_class = BookingSerializer
+    # serializer_class = BookingSerializer
+    serializer_class = BookingDetailsSerializer
+
     
 class BookingSubmitAPIView(APIView):
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request):
-        times = [
-            "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM"
-        ]
+
         today = datetime.now()
         minDate = today.strftime('%Y-%m-%d')
         deltatime = today + timedelta(days=21)
@@ -48,42 +50,43 @@ class BookingSubmitAPIView(APIView):
         maxDate = strdeltatime
 
         day = request.data.get('day')
-        service = request.data.get('service')
+        # service = request.data.get('service')
+        description= request.data.get('description')
         craftsman = request.data.get("Craftsman")
         time = request.data.get("time")
-        date = day
+        # date = day
         existing_booking = Booking.objects.filter(day=day,time=time,craftsman=craftsman)
 
-        if service != None:
-            if day <= maxDate and day >= minDate:
-                if date != 'Friday' :
-                    if isCraftsmanValid(craftsman):
-                        if not existing_booking:
-                            custmer_profile = CustmerProfile.objects.get(user=request.user)
-                            craftsman_obj = CraftsmanProfile.objects.get(pk=craftsman)
-                            booking = Booking.objects.create(
-                                custmer=custmer_profile,
-                                craftsman=craftsman_obj,
-                                day=day,
-                                time=time,
-                                description=service
-                            )
-                            booking.save()
-                            return Response({"status": "Booking created successfully!"}, status=status.HTTP_201_CREATED)
-                        else:
-                            return Response({"status": "The selected craftsman is not available at this time."}, status=status.HTTP_400_BAD_REQUEST)
+        # if service != None:
+        if day <= maxDate and day >= minDate:
+            # if date != 'Friday' :
+                if isCraftsmanValid(craftsman):
+                    if not existing_booking:
+                        custmer_profile = CustmerProfile.objects.get(user=request.user)
+                        craftsman_obj = CraftsmanProfile.objects.get(pk=craftsman)
+                        booking = Booking.objects.create(
+                            custmer=custmer_profile,
+                            craftsman=craftsman_obj,
+                            day=day,
+                            time=time,
+                            description=description
+                        )
+                        booking.save()
+                        return Response({"status": "Booking created successfully!"}, status=status.HTTP_201_CREATED)
                     else:
-                        return Response({"status": "The selected craftsman is not valid."}, status=status.HTTP_400_BAD_REQUEST)
+                        return Response({"status": "The selected craftsman is not available at this time."}, status=status.HTTP_400_BAD_REQUEST)
                 else:
-                    return Response({"status": "This day is not available for booking."}, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                return Response({"status": "This date is not available for booking."}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response({"status": "The selected craftsman is not valid."}, status=status.HTTP_400_BAD_REQUEST)
+            # else:
+            #     return Response({"status": "This day is not available for booking."}, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({"status": "Please Select A Service!"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"status": "This date is not available for booking."}, status=status.HTTP_400_BAD_REQUEST)
+        # else:
+        #     return Response({"status": "Please Select A Service!"}, status=status.HTTP_400_BAD_REQUEST)
 
 class CreateBookingView(CreateAPIView):
     '''
-    Create new book 
+    Create new booking 
     '''
     queryset = Booking.objects.all()
     serializer_class = CreateBookingSerializer
@@ -94,31 +97,31 @@ class CreateBookingView(CreateAPIView):
         return serializer.save(custmer=custmer)
 
 
-class UserBookingView(ListAPIView):
-    '''
-    Get all bookings related for any user 
-    '''
-    def get_queryset(self):
-        if self.request.user.role == 'CRAFTSMAN':
-            craftsman = CraftsmanProfile.objects.get(user=self.request.user.id)
-            data = Booking.objects.filter(craftsman=craftsman)
-            return data
+# class UserBookingView(ListAPIView):
+#     '''
+#     Get all bookings related for any user 
+#     '''
+#     def get_queryset(self):
+#         if self.request.user.role == 'CRAFTSMAN':
+#             craftsman = CraftsmanProfile.objects.get(user=self.request.user.id)
+#             data = Booking.objects.filter(craftsman=craftsman)
+#             return data
 
-        elif self.request.user.role == 'CUSTMER':
-            custmer = CustmerProfile.objects.get(user=self.request.user.id)
-            data = Booking.objects.filter(custmer=custmer)
-            return data
+#         elif self.request.user.role == 'CUSTMER':
+#             custmer = CustmerProfile.objects.get(user=self.request.user.id)
+#             data = Booking.objects.filter(custmer=custmer)
+#             return data
 
-        else:
-            return Booking.objects.all()
-    serializer_class = BookingSerializer
+#         else:
+#             return Booking.objects.all()
+#     serializer_class = BookingSerializer
 
 
 
-class BookingRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
-    # permission_classes = [IsCustmerOrReadOnly]
-    queryset = Booking.objects.all()
-    serializer_class = BookingDetailsSerializer
+# class BookingRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+#     permission_classes = [IsCustmerOrReadOnly]
+#     queryset = Booking.objects.all()
+#     serializer_class = BookingDetailsSerializer
 
 
 
